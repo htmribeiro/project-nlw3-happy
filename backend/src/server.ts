99d93be93ff1deaff757691f54f82f86e5c6b611
Hamilton Ribeiro <htadeu@hotmail.com>
2020-10-15 17:53:28 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express(); //criação da aplicação
 
 app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello World' });
+    return response.json({ message: 'Hello World'  });
 });
 
 app.listen(3333); // localhost:3333
