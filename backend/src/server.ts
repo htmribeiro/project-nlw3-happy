c54d64b5bbf2f7579b18bdd04172cb676bf07cbd
Hamilton Ribeiro <htadeu@hotmail.com>
2020-10-16 21:30:37 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,19 @@ import express from 'express';
 
 const app = express(); //criação da aplicação
 
+// Criaçao da Rota
+// Recurso = users
+// Método HTTP = GET, POST, PUT, DELETE
+
+// GET = Buscar uma informação (Lista, Item)
+// POST = Criando uma informação
+// PUT = Editando uma informação
+// DELETE = Deletando uma informação
+
+// Parâmetros
+// Query Params: http://localhost:3333/users?search=diego
+// Route Params: 
+
 app.get('/users', (request, response) => {
     return response.json({ message: 'Hello World'  });
 });
