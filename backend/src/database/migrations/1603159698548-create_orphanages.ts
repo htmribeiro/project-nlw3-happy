45ce8da99df8c466ebaede296feceba5007d929c
Hamilton Ribeiro <htadeu@hotmail.com>
2020-10-20 00:12:55 -0300

    Update 1603159698548-create_orphanages.ts
    1. Implementando o método `up`
    2. Implementando o método `down`

--- backend/src/database/migrations/1603159698548-create_orphanages.ts
+++ backend/src/database/migrations/1603159698548-create_orphanages.ts
@@ -1,11 +1,54 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createOrphanages1603159698548 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'orphanages',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true, // essa coluna sempre será um número positivo
+          isPrimary: true, // indica que essa é a minha coluna PK
+          isGenerated: true, // essa coluna será gerada automaticamente
+          generationStrategy: 'increment', // defino a lógica incremental da coluna 'isGenererated' como auto-incremental
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'latitude',
+          type: 'decimal',
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: 'longitude',
+          type: 'decimal',
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: 'about',
+          type: 'text',
+        },
+        {
+          name: 'instructions',
+          type: 'text',
+        },
+        {
+          name: 'open_on_weekends',
+          type: 'boolean',
+          default: false,
+        }
+      ],
+    }))
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
+  }
 
 }
