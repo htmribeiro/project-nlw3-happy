845e16788013932f5b28ef452b164082403e2124
Hamilton Ribeiro <htadeu@hotmail.com>
2020-10-19 23:27:24 -0300

    Create 1603159698548-create_orphanages.ts

---
+++ backend/src/database/migrations/1603159698548-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1603159698548 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
