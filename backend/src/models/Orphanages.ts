bfe6fdd5435a78e8c475b7b344a4774b8b90660a
Hamilton Ribeiro <htadeu@hotmail.com>
2020-10-20 21:09:22 -0300

    Create Orphanages.ts
    Criando orfanato sem imagem

---
+++ backend/src/models/Orphanages.ts
@@ -0,0 +1,17 @@
+export default class Orphanage {
+  id: number;
+
+  name: string;
+
+  latitude: number;
+
+  longitude: number;
+
+  about: string;
+
+  intructions: string;
+
+  opening_hours: string;
+
+  open_on_weekends: boolean;
+}
